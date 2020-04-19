bffb52e4a9ed6e16c6a9f56207d70f32cbf15d9c
leonid.golenkov <leonid.golenkov@gmail.com>
2020-04-19 20:19:14 +0300

    [add] display

---
+++ src/display.h
@@ -0,0 +1,31 @@
+#ifndef DISPLAY_H
+#define DISPLAY_H
+
+#include <stdint.h>
+#include <stdbool.h>
+#include <SDL2/SDL.h>
+
+extern SDL_Window *window;
+extern SDL_Renderer *renderer;
+
+extern uint32_t *color_buffer;
+extern SDL_Texture *color_buffer_texture;
+
+extern int window_width;
+extern int window_height;
+
+
+bool intialize_window(void);
+
+void destroy_window(void);
+
+void clear_color_buffer(uint32_t color);
+
+void render_color_buffer(void);
+
+void draw_grid(int grid_size, uint32_t color);
+
+void draw_rect(int x, int y, int width, int height, uint32_t color);
+
+
+#endif
--- src/main.c
+++ src/main.c
@@ -1,55 +1,7 @@
 #include <stdio.h>
-#include <stdint.h>
-#include <stdbool.h>
-#include <SDL2/SDL.h>
+#include "display.h"
 
-bool debug_print_flag = false;
 bool is_running = false;
-
-SDL_Window *window = NULL;
-SDL_Renderer *renderer = NULL;
-
-uint32_t *color_buffer = NULL;
-SDL_Texture *color_buffer_texture = NULL;
-
-int window_width = 256;
-int window_height = 256;
-
-bool intialize_window(void) {
-    if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
-        fprintf(stderr, "Error intializing SDL.\n");
-        return false;
-    }
-
-    SDL_DisplayMode display_mode;
-    SDL_GetCurrentDisplayMode(0, &display_mode);
-//    window_width = display_mode.w / 4 * 3;
-//    window_height = display_mode.h / 4 * 3;
-
-    // create a sdl window
-    Uint32 flags = SDL_WINDOW_BORDERLESS | SDL_WINDOW_ALWAYS_ON_TOP;
-    window = SDL_CreateWindow(
-            NULL,
-            SDL_WINDOWPOS_CENTERED,
-            SDL_WINDOWPOS_CENTERED,
-            window_width,
-            window_height,
-            flags
-    );
-    if (!window) {
-        fprintf(stderr, "Error creating SDL window.\n");
-        return false;
-    }
-
-    // create a sdl renderer
-    renderer = SDL_CreateRenderer(window, -1, 0);
-    if (!renderer) {
-        fprintf(stderr, "Error creating SDL renderer.\n");
-        return false;
-    }
-
-    return true;
-}
 
 void setup(void) {
     // allocate the required memory in bytes to hold the color buffer
@@ -84,69 +36,6 @@ void update(void) {
     //todo
 }
 
-void draw_grid(int grid_size, uint32_t color) {
-    for (int y = 0; y < window_height; y++) {
-        for (int x = 0; x < window_width; x++) {
-            if (y % grid_size == 0 || x % grid_size == 0) {
-                color_buffer[(window_width * y) + x] = color;
-            }
-        }
-    }
-}
-
-void render_color_buffer() {
-    SDL_UpdateTexture(
-            color_buffer_texture,
-            NULL,
-            color_buffer,
-            (int) (window_width * sizeof(uint32_t))
-    );
-
-    SDL_RenderCopy(renderer, color_buffer_texture, NULL, NULL);
-}
-
-void clear_color_buffer(uint32_t color) {
-    for (int y = 0; y < window_height; y++) {
-        for (int x = 0; x < window_width; x++) {
-            color_buffer[(window_width * y) + x] = color;
-        }
-    }
-}
-
-bool is_rect_outside(int x, int y, int width, int height) {
-    return (x + width <= 0 || y + height <= 0 || x >= window_width || y >= window_height);
-}
-
-void draw_rect(int x, int y, int width, int height, uint32_t color) {
-    if (is_rect_outside(x, y, width, height)) {
-        return;
-    }
-
-    if (x < 0) {
-        width += x;
-        x = 0;
-    }
-
-    if (y < 0) {
-        height += y;
-        y = 0;
-    }
-
-    if (x + width > window_width) {
-        width = window_width - x;
-    }
-
-    if (y + height > window_height) {
-        height = window_height - y;
-    }
-
-    for (int j = y; j < y + height; j++) {
-        for (int i = x; i < x + width; i++) {
-            color_buffer[(window_width * j) + i] = color;
-        }
-    }
-}
-
 void render(void) {
 //    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
     SDL_RenderClear(renderer);
@@ -163,15 +52,7 @@ void render(void) {
     SDL_RenderPresent(renderer);
 }
 
-void destroy_window(void) {
-    free(color_buffer);
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window);
-    SDL_Quit();
-}
-
 int main(void) {
-
     is_running = intialize_window();
 
     setup();

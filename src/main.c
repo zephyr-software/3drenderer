45145bea94f0609ef8c196f75ba3019a8f62c44e
leonid.golenkov <leonid.golenkov@gmail.com>
2020-04-28 09:38:31 +0300

    [add] dda line drawing algorithm

--- src/main.c
+++ src/main.c
@@ -112,6 +112,11 @@ void render(void) {
     for (int i = 0; i < N_MESH_FACES; i++) {
         triangle_t triangle = triangles_to_render[i];
 
+        draw_triangle(triangle.points[0].x, triangle.points[0].y,
+                      triangle.points[1].x, triangle.points[1].y,
+                      triangle.points[2].x, triangle.points[2].y,
+                      0xFFFFFF00);
+
         draw_rect(triangle.points[0].x, triangle.points[0].y, 4, 4, 0xFFFF00FF);
         draw_rect(triangle.points[1].x, triangle.points[1].y, 4, 4, 0xFFFF00FF);
         draw_rect(triangle.points[2].x, triangle.points[2].y, 4, 4, 0xFFFF00FF);

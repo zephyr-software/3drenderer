b3f6733745917500922cb989a73fbdaa855facf2
leonid <leonid.golenkov@gmail.com>
2020-05-03 21:52:14 +0300

    [add] vector scalar multiplication and division

--- src/vector.c
+++ src/vector.c
@@ -24,6 +24,24 @@ vec2_t vec2_sub(vec2_t vector_a, vec2_t vector_b) {
     return result;
 }
 
+vec2_t vec2_mul(vec2_t vector, float factor) {
+    vec2_t result = {
+            .x = vector.x * factor,
+            .y = vector.y * factor
+    };
+
+    return result;
+}
+
+vec2_t vec2_div(vec2_t vector, float factor) {
+    vec2_t result = {
+            .x = vector.x / factor,
+            .y = vector.y / factor
+    };
+
+    return result;
+}
+
 
 float vec3_length(vec3_t vector) {
     return sqrt(vector.x * vector.x + vector.y * vector.y + vector.z * vector.z);
@@ -44,6 +62,26 @@ vec3_t vec3_sub(vec3_t vector_a, vec3_t vector_b) {
             .x = vector_a.x - vector_b.x,
             .y = vector_a.y - vector_b.y,
             .z = vector_a.z - vector_b.z
+    };
+
+    return result;
+}
+
+vec3_t vec3_mul(vec3_t vector, float factor) {
+    vec3_t result = {
+            .x = vector.x * factor,
+            .y = vector.y * factor,
+            .z = vector.z * factor
+    };
+
+    return result;
+}
+
+vec3_t vec3_div(vec3_t vector, float factor) {
+    vec3_t result = {
+            .x = vector.x / factor,
+            .y = vector.y / factor,
+            .z = vector.z / factor
     };
 
     return result;
--- src/vector.h
+++ src/vector.h
@@ -20,6 +20,10 @@ vec2_t vec2_add(vec2_t vector_a, vec2_t vector_b);
 
 vec2_t vec2_sub(vec2_t vector_a, vec2_t vector_b);
 
+vec2_t vec2_mul(vec2_t vector, float factor);
+
+vec2_t vec2_div(vec2_t vector, float factor);
+
 
 float vec3_length(vec3_t vector);
 
@@ -27,11 +31,16 @@ vec3_t vec3_add(vec3_t vector_a, vec3_t vector_b);
 
 vec3_t vec3_sub(vec3_t vector_a, vec3_t vector_b);
 
+vec3_t vec3_mul(vec3_t vector, float factor);
+
+vec3_t vec3_div(vec3_t vector, float factor);
+
 
 vec3_t vec3_rotate_x(vec3_t vector, float angle);
 
 vec3_t vec3_rotate_y(vec3_t vector, float angle);
 
 vec3_t vec3_rotate_z(vec3_t vector, float angle);
+
 
 #endif

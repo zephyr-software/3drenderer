75ca3126abada94d18cce5872a4aa02ade886b25
leonid <leonid.golenkov@gmail.com>
2020-05-03 18:53:23 +0300

    [add] vector addition and subtraction

--- src/vector.c
+++ src/vector.c
@@ -6,9 +6,49 @@ float vec2_length(vec2_t vector) {
     return sqrt(vector.x * vector.x + vector.y * vector.y);
 }
 
+vec2_t vec2_add(vec2_t vector_a, vec2_t vector_b) {
+    vec2_t result = {
+            .x = vector_a.x + vector_b.x,
+            .y = vector_a.y + vector_b.y
+    };
+
+    return result;
+}
+
+vec2_t vec2_sub(vec2_t vector_a, vec2_t vector_b) {
+    vec2_t result = {
+            .x = vector_a.x - vector_b.x,
+            .y = vector_a.y - vector_b.y
+    };
+
+    return result;
+}
+
+
 float vec3_length(vec3_t vector) {
     return sqrt(vector.x * vector.x + vector.y * vector.y + vector.z * vector.z);
 }
+
+vec3_t vec3_add(vec3_t vector_a, vec3_t vector_b) {
+    vec3_t result = {
+            .x = vector_a.x + vector_b.x,
+            .y = vector_a.y + vector_b.y,
+            .z = vector_a.z + vector_b.z
+    };
+
+    return result;
+}
+
+vec3_t vec3_sub(vec3_t vector_a, vec3_t vector_b) {
+    vec3_t result = {
+            .x = vector_a.x - vector_b.x,
+            .y = vector_a.y - vector_b.y,
+            .z = vector_a.z - vector_b.z
+    };
+
+    return result;
+}
+
 
 vec3_t vec3_rotate_x(vec3_t vector, float angle) {
     vec3_t rotated_vector = {
@@ -39,4 +79,3 @@ vec3_t vec3_rotate_z(vec3_t vector, float angle) {
 
     return rotated_vector;
 }
-
--- src/vector.h
+++ src/vector.h
@@ -1,6 +1,7 @@
 #ifndef VECTOR_H
 #define VECTOR_H
 
+
 typedef struct {
     float x;
     float y;
@@ -12,9 +13,20 @@ typedef struct {
     float z;
 } vec3_t;
 
+
 float vec2_length(vec2_t vector);
 
+vec2_t vec2_add(vec2_t vector_a, vec2_t vector_b);
+
+vec2_t vec2_sub(vec2_t vector_a, vec2_t vector_b);
+
+
 float vec3_length(vec3_t vector);
+
+vec3_t vec3_add(vec3_t vector_a, vec3_t vector_b);
+
+vec3_t vec3_sub(vec3_t vector_a, vec3_t vector_b);
+
 
 vec3_t vec3_rotate_x(vec3_t vector, float angle);
 

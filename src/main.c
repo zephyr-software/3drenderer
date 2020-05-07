51eca1dbffb1c6af9c5337739d5cf8f7cb8def69
leonid <leonid.golenkov@gmail.com>
2020-05-07 03:21:59 +0300

    [add] drawing the flat-top triangle

--- src/main.c
+++ src/main.c
@@ -157,15 +157,15 @@ void render(void) {
     for (int i = 0; i < num_triangles; i++) {
         triangle_t triangle = triangles_to_render[i];
 
+        draw_filled_triangle(triangle.points[0].x, triangle.points[0].y,
+                             triangle.points[1].x, triangle.points[1].y,
+                             triangle.points[2].x, triangle.points[2].y,
+                             0xFF777700);
+
         draw_triangle(triangle.points[0].x, triangle.points[0].y,
                       triangle.points[1].x, triangle.points[1].y,
                       triangle.points[2].x, triangle.points[2].y,
                       0xFFFFFF00);
-
-        draw_filled_triangle(triangle.points[0].x, triangle.points[0].y,
-                             triangle.points[1].x, triangle.points[1].y,
-                             triangle.points[2].x, triangle.points[2].y,
-                             0xFF777777);
 
         draw_rect(triangle.points[0].x, triangle.points[0].y, 4, 4, 0xFFFF00FF);
         draw_rect(triangle.points[1].x, triangle.points[1].y, 4, 4, 0xFFFF00FF);
--- src/triangle.c
+++ src/triangle.c
@@ -8,6 +8,19 @@ void swap(int *a, int *b) {
     *b = tmp;
 }
 
+///////////////////////////////////////////////////////////////////////////////
+// Draw a filled a triangle with a flat bottom
+///////////////////////////////////////////////////////////////////////////////
+//
+//        (x0,y0)
+//          / \
+//         /   \
+//        /     \
+//       /       \
+//      /         \
+//  (x1,y1)------(x2,y2)
+//
+///////////////////////////////////////////////////////////////////////////////
 void fill_flat_bottom_triangle(int x0, int y0, int x1, int y1, int x2, int y2, uint32_t color) {
     float inv_slope_1 = (float) (x1 - x0) / (y1 - y0);
     float inv_slope_2 = (float) (x2 - x0) / (y2 - y0);
@@ -23,11 +36,59 @@ void fill_flat_bottom_triangle(int x0, int y0, int x1, int y1, int x2, int y2, u
     }
 }
 
+///////////////////////////////////////////////////////////////////////////////
+// Draw a filled a triangle with a flat top
+///////////////////////////////////////////////////////////////////////////////
+//
+//  (x0,y0)------(x1,y1)
+//      \         /
+//       \       /
+//        \     /
+//         \   /
+//          \ /
+//        (x2,y2)
+//
+///////////////////////////////////////////////////////////////////////////////
 void fill_flat_top_triangle(int x0, int y0, int x1, int y1, int x2, int y2, uint32_t color) {
+    // Find the two slopes (two triangle legs)
+    float inv_slope_1 = (float) (x2 - x0) / (y2 - y0);
+    float inv_slope_2 = (float) (x2 - x1) / (y2 - y1);
 
+    // Start x_start and x_end from the bottom vertex (x2,y2)
+    float x_start = x2;
+    float x_end = x2;
+
+    // Loop all the scanlines from bottom to top
+    for (int y = y2; y >= y0; y--) {
+        draw_line(x_start, y, x_end, y, color);
+        x_start -= inv_slope_1;
+        x_end -= inv_slope_2;
+    }
 }
 
-
+///////////////////////////////////////////////////////////////////////////////
+// Draw a filled triangle with the flat-top/flat-bottom method
+// We split the original triangle in two, half flat-bottom and half flat-top
+///////////////////////////////////////////////////////////////////////////////
+//
+//          (x0,y0)
+//            / \
+//           /   \
+//          /     \
+//         /       \
+//        /         \
+//   (x1,y1)------(Mx,My)
+//       \_           \
+//          \_         \
+//             \_       \
+//                \_     \
+//                   \    \
+//                     \_  \
+//                        \_\
+//                           \
+//                         (x2,y2)
+//
+///////////////////////////////////////////////////////////////////////////////
 void draw_filled_triangle(int x0, int y0, int x1, int y1, int x2, int y2, uint32_t color) {
     if (y0 > y1) {
         swap(&y0, &y1);
@@ -44,9 +105,21 @@ void draw_filled_triangle(int x0, int y0, int x1, int y1, int x2, int y2, uint32
         swap(&x0, &x1);
     }
 
-    int my = y1;
-    int mx = (x2 - x0) * (y1 - y0) / (y2 - y0) + x0;
+    if (y1 == y2) {
+        // Draw flat-bottom triangle
+        fill_flat_bottom_triangle(x0, y0, x1, y1, x2, y2, color);
+    } else if (y0 == y1) {
+        // Draw flat-top triangle
+        fill_flat_top_triangle(x0, y0, x1, y1, x2, y2, color);
+    } else {
+        // Calculate the new vertex (Mx,My) using triangle similarity
+        int My = y1;
+        int Mx = (((x2 - x0) * (y1 - y0)) / (y2 - y0)) + x0;
 
-    fill_flat_bottom_triangle(x0, y0, x1, y1, mx, my, color);
-    fill_flat_top_triangle(x1, y1, mx, my, x2, y2, color);
+        // Draw flat-bottom triangle
+        fill_flat_bottom_triangle(x0, y0, x1, y1, Mx, My, color);
+
+        // Draw flat-top triangle
+        fill_flat_top_triangle(x1, y1, Mx, My, x2, y2, color);
+    }
 }
